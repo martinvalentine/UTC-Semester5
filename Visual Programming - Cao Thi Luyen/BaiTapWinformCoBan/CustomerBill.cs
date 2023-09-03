6e0956e919ea95c858b7e15380bd1e177ca9d2b1
Martin Valentine <nguythequang200.com>
2023-09-03 22:32:03 +0700

    Create CustomerBill class

---
+++ Visual Programming - Cao Thi Luyen/BaiTapWinformCoBan/CustomerBill.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BaiTapWinformCoBan
+{
+    internal class CustomerBill
+    {
+        string cusID, name, addr;
+        int lastmth, thismth;
+        DateTime dayReadMeter;
+
+        public CustomerBill()
+        {
+
+        }
+
+        public CustomerBill(string cusID, string name, string addr, DateTime dayReadMeter, int lastmth, int thismth)
+        {
+            this.cusID = cusID;
+            this.name = name;
+            this.addr = addr;
+            this.dayReadMeter = dayReadMeter;
+            this.lastmth = lastmth;
+            this.thismth = thismth;
+        }
+
+        public override string ToString()
+        {
+            return "Customer: " + cusID + " " + name + " " + addr + " " + dayReadMeter.ToString() + " " + lastmth.ToString() + " " + thismth.ToString();
+        }
+    }
+}
