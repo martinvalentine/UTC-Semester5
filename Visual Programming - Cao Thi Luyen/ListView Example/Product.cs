7210aaa2918374c5fe110845990204a5a20ffe8f
Martin Valentine <nguythequang200.com>
2023-09-05 10:43:56 +0700

    Create Product class

---
+++ Visual Programming - Cao Thi Luyen/ListView Example/Product.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListView_Example
+{
+    internal class Product
+    {
+        string id, name;
+        int quantity;
+
+        public Product(string id = "", string name = "", int quantity = 0)
+        {
+            this.id = id;
+            this.name = name;
+            this.quantity = quantity;
+        }
+    }
+}
