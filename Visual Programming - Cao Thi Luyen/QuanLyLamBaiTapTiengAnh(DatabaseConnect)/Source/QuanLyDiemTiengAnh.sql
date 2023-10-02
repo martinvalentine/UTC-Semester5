1883e066da06d02cf695f5325d5fc14547d93a26
Martin Valentine <nguythequang200@gmail.com>
2023-10-03 02:45:53 +0700

    add sql database files

---
+++ Visual Programming - Cao Thi Luyen/QuanLyLamBaiTapTiengAnh(DatabaseConnect)/Source/QuanLyDiemTiengAnh.sql
@@ -0,0 +1,44 @@
+﻿
+USE QuanLyDiemTiengAnh
+
+CREATE TABLE tHocSinh
+(
+	MaHS NVARCHAR(5) NOT NULL,
+	HoTen NVARCHAR(50),
+	Lop NVARCHAR(10),
+
+	CONSTRAINT pk_tHocSinh PRIMARY KEY(MaHS)
+)
+
+CREATE TABLE tPhieuDiem
+(
+	MaHS NVARCHAR(5) NOT NULL,
+	Diem INT,
+	TGBatDau DATETIME,
+	TGKetThuc DATETIME,
+	MaBai NVARCHAR(5) NOT NULL,
+	SoLanLam INT,
+
+	CONSTRAINT PK_tPhieuDiem PRIMARY KEY CLUSTERED 
+	(
+		MaHS ASC,
+		MaBai ASC
+	)
+)
+
+CREATE TABLE tDangBaiTap
+(
+	DangBaiTap NVARCHAR(50),
+	TenBai NVARCHAR(50),
+	MaBai NVARCHAR(5) NOT NULL,
+
+	CONSTRAINT pk_tDangBaiTap PRIMARY KEY(MaBai)
+)
+
+-- Vì ở đây 2 khóa ở tPhieuDiem vừa là khóa chính khóa phụ nên ta sẽ tạo ràng buộc với 2 bảng còn lại
+
+ALTER TABLE [dbo].[tPhieuDiem]  WITH CHECK ADD CONSTRAINT [FK_tPhieuDiem_tHocSinh] FOREIGN KEY([MaHS])
+REFERENCES [dbo].[tHocSinh] ([MaHS])
+
+ALTER TABLE [dbo].[tPhieuDiem]  WITH CHECK ADD CONSTRAINT [FK_tPhieuDiem_tDangBaiTap] FOREIGN KEY([MaBai])
+REFERENCES [dbo].[tDangBaiTap] ([MaBai])
